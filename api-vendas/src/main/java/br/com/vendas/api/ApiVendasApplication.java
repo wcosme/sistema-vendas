841e71aa79e56cee505c5f660470627a176d2cee
Wallace Gonçalves <wallacecosme@gmail.com>
2022-01-04 22:41:11 -0300

    Setup inicial do projeto

---
+++ api-vendas/src/main/java/br/com/vendas/api/ApiVendasApplication.java
@@ -0,0 +1,13 @@
+package br.com.vendas.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVendasApplication.class, args);
+	}
+
+}
